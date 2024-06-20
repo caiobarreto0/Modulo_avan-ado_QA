46ea706bf6b2d06e5d9fa4a793355bb3dcf8ab20
Caio <caiobarreto00@gmail.com>
2024-06-20 11:00:22 -0300

    Aula 01

---
+++ Aula_01/input.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Sua idade: "))
+print(type(idade))
+idade_futura = idade + 3
+
+print(f"O seu nome  é {nome} e a idade da gay é:{idade}, idade futura {idade_futura}")
