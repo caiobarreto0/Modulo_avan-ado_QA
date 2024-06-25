db3d4ff17434569fe5732578bc772c27f806ea45
Caio <caiobarreto00@gmail.com>
2024-06-25 17:27:44 -0300

    Atividade lista tuplas

---
+++ Aula_03/lista_tuplas.py
@@ -0,0 +1,21 @@
+listaCarros = ("cruze, onix, hb20, jetta, golf")
+
+listaAutos = list(listaCarros)
+
+print(listaAutos)
+
+listaAutos.extend(["celta", "fusca"])
+
+print(listaAutos)
+
+listaAutos.pop()
+
+print(listaAutos)
+
+print(listaAutos[0])
+
+totalElementos = len(listaAutos)
+
+print(totalElementos)
+
+
